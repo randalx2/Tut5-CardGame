9a0c7b5f7578002b5b2ddbd753331e339a61760c
randalx2 <pmisthry@gmail.com>
2016-03-18 01:05:05 +0200

    Created necessary header files

---
+++ Tut5-CardGame/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <cmath>
+#include <ctime>
+
+#include "Card.h"
+#include "DeckOfCards.h"
+
+int main()
+{
+
+
+	system("PAUSE");
+	return 0;
+}
