dc68d828f600a01b17a6ae7223d0ab226bff5940
randalx2 <pmisthry@gmail.com>
2016-03-18 01:57:51 +0200

    Created DeckOfCards declaration

--- Tut5-CardGame/DeckOfCards.h
+++ Tut5-CardGame/DeckOfCards.h
@@ -2,3 +2,25 @@
 //COMPUTER METHODS 3 TUT5-CARDGAME
 //18 MARCH 2016
 /********************************************************************************************************************/
+#pragma once
+#ifndef DECKOFCARDS_H
+#define DECKOFCARDS_H
+#include <string>
+
+#include "Card.h"
+
+using namespace std;
+
+class DeckOfCards{
+private: int cardsRemaining;
+		 Card *cardPtr = NULL;  //pointer for card dynamic array
+public: DeckOfCards();
+		~DeckOfCards();
+		DeckOfCards reset();  //Resets cards back to original deck
+		void shuffle();
+		Card draw();  //returns card and removes it using DMA
+		Card peek(); //returns card but doesn't remove it
+		int numberOfCards();
+};
+
+#endif
